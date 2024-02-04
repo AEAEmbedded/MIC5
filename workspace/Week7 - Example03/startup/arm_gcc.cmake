9e2b274da953d6abf9c701902458a95a864183b0
Hoog-V <vo.hogeweij@student.han.nl>
2024-02-04 15:11:36 +0100

    -add Windows compatibility change from Richard Kroesen at issue #1 preventing target hardfaults

--- workspace/Week7 - Example03/startup/arm_gcc.cmake
+++ workspace/Week7 - Example03/startup/arm_gcc.cmake
@@ -1,3 +1,19 @@
+#[[
+  Copyright 2023 (C) Richard Kroesen <kroesen.ri@gmail.com> & Victor Hogeweij <hogeweyv@gmail.com>
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+]]
+
 set(CMAKE_SYSTEM_NAME Generic)
 set(CMAKE_SYSTEM_PROCESSOR ARM)
 if(${CMAKE_VERSION} VERSION_LESS "3.16.0")
@@ -16,7 +32,6 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 
 add_compile_options(-mcpu=${MCPU} -mthumb -mthumb-interwork)
-
 add_definitions(-DCPU_MKL25Z128VLK4 
                 -DFRDM_KL25Z 
                 -DFREEDOM
@@ -35,7 +50,15 @@ add_definitions(-DCPU_MKL25Z128VLK4
                 -fdata-sections 
                 -fmerge-constants
                 -mapcs)
+                
+add_link_options(-mthumb
+                -mcpu=${MCPU}
+                -specs=nano.specs 
+                -Wl,--gc-sections
+                --specs=nosys.specs
+                -Wl,--print-memory-usage
+                -lm
+                -Wl,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map)
 
 set(LINKER_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/linker_script.ld)
-add_link_options(-lm -Wl,-gc-sections,--print-memory-usage,--no-warn-rwx-segments,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map --specs=nano.specs -Xlinker -static -mthumb)
 add_link_options(-T ${LINKER_SCRIPT} -static)
